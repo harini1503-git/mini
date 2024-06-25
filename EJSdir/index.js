09710d193892c8c1c93ca0ebdd034d48525664a1
Harini Mudaliar <harinimudaliar1503@gmail.com>
2024-06-25 16:26:55 +0530

    add new commits

---
+++ EJSdir/index.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const app= express();
+const path= require("path");
+let port= 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res)=>{
+    res.render("home");
+})
+
+app.get("/instagram/:username",(req, res)=>{
+    let {username}= req.params;
+    const data= require("./instadata.json");
+    let instadata= data[username];
+    res.render("instagram.ejs", {instadata});
+    
+})
+
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+})
